1935166120aec85cfd3082a9b800bdc5e668d69a
Usama Hussain <uhussain@macrosoftinc.com>
2024-04-01 17:33:30 +0500

    update and delete api

--- routes/products.mjs
+++ routes/products.mjs
@@ -1,17 +1,21 @@
 import express from 'express'
 import Products from '../modals/products.mjs'
 
-const router=express.Router()
+const router = express.Router()
 
-router.get('/',async(req,res)=>{
-    // start db operations
-    const getProducts = await Products.find()
-    res.send({message:'Products Fetched Successfully!',data:getProducts})
+router.get('/', async (req, res) => {
+    try {
+        // start db operations
+        const getProducts = await Products.find()
+        res.send({ message: 'Products Fetched Successfully!', data: getProducts })
+    } catch (error) {
+        res.send({ message: error.message })
+    }
 })
 
-router.post('/add',async(req,res)=>{
-    // start db operations
+router.post('/add', async (req, res) => {
     try {
+        // start db operations
         await Products.create(req.body)
         res.send({ message: 'Product added successfully!' })
     } catch (error) {
@@ -19,14 +23,34 @@ router.post('/add',async(req,res)=>{
     }
 })
 
-router.put('/update',(req,res)=>{
+router.put('/update', async (req, res) => {
     // start db operations
-    res.send({message:'Record updated successfully!'})
+    try {
+        const { _id, title, amount, description } = req.body;
+        let updatedProduct= await Products.findOneAndUpdate(
+            { _id },
+            { $set: { title, amount, description } },
+            { new: true }
+        );
+        res.send({ message: 'Record updated successfully!',data: updatedProduct });
+    } catch (error) {
+        res.send({ message: error.message })
+    }
 })
 
-router.delete('/delete',(req,res)=>{
+router.delete('/delete/:_id', async(req, res) => {
     // start db operations
-    res.send({message:'Record deleted successfully!'})
+    try {
+        const { _id } = req.params;
+        let deletedRecord = await Products.findOneAndDelete( { _id } );
+        if(!deletedRecord){
+            res.send({ message: 'Record not found!' })
+            return
+        }
+        res.send({ message: 'Record deleted successfully!' })
+    } catch (error) {
+        res.send({ message: 'Record not deleted!' })
+    }
 })
 
 export default router
