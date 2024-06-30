a12fbff401956075d7ad2ad426ea9b590152759f
Usama Hussain <usamaahussain23@gmail.com>
2024-06-30 15:19:34 +0500

    fix cors issue using cors package

--- index.mjs
+++ index.mjs
@@ -1,16 +1,19 @@
 import express from 'express'
 import routes from './routes/index.mjs'
 import mongodb from './config/db.mjs'
+import cors from 'cors'
 
 const app = express()
 
 
 /* TO LOOK AND RESERACH ON THIS CURRENTLY IT IS DONE FOR CORSE-ORIGIN-POLICY-ERROR  */
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+//   });
 
   
 app.listen(3001, () => {
@@ -22,5 +25,5 @@ mongodb.connection.once('open',()=>{
 })
 
 app.use(express.json()) //Backend ko batana ka tumhari bodies ki type JSON hogi
-
+app.use(cors())
 app.use('/',routes)
